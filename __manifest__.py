2bf215affa8c6f834588292bbba9ee49eca71da9
Roberto Figueroa <marroquinroberto3@gmail.com>
2025-06-17 10:06:35 -0600

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Mi módulo custom",
+    'version': '1.0',
+    'depends': ['web'],
+    'data': [
+        'views/custom_external_layout.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
